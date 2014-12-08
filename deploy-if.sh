1e029db06ae9b8406d15bfaf2cc00adc55925ed5
Kristinn Sigurðsson <kris.sigur@gmail.com>
2014-12-08 15:00:27 +0000

    Creating

---
+++ deploy-if.sh
@@ -0,0 +1,20 @@
+mvn install --settings target/travis/settings.xml -B
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+#
+# Attempt to deploy SNAPSHOTS.
+#
+# We deliberatly ignore failed snapshot deployments, as this can happen during the release procedure.
+#
+# Logic for ignoring pull requests:
+if [ $TRAVIS_SECURE_ENV_VARS == 'true' ]; then
+echo "TRAVIS_SECURE_ENV_VARS is true, attempting deployment..."
+
+if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
+  echo "TRAVIS_PULL_REQUEST is false. Deploying..."
+  mvn deploy --settings target/travis/settings.xml -B -DskipTests
+
+fi
+fi
